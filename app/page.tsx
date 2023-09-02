44cbc6eb63bea37cc8193cde43c1a8b354aa001c
oniasfilho <onias.filho@gmail.com>
2023-09-02 09:55:24 -0400

    minor correction

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,7 @@
 'use client'
-
 import Pricing from './components/Pricing'
 
-
-
-
-
-
 export default function Home() {
-
-
-
   return (
     <main className="w-full min-h-screen flex flex-col items-center justify-center bg-main-bg-color text-regular-text overflow-x-hidden">
       <Pricing />
